e34f9a2efd90d80c1213029d2e73fc0c27969414
Darren Kim <dkim555@yahoo.com>
2023-01-18 13:42:47 -0800

    else conditional

--- recurion-problem2/script.js
+++ recurion-problem2/script.js
@@ -1,2 +1,18 @@
 // Write a JavaScript function to compute the sum of an array of integers.
 
+const nums = [1, 5, 4, 3, 6];
+
+function sumOfArr(arr) {
+    // the recursion is done once the array hits 0
+    if (arr.length < 1) {
+        // return 0 so that it stops adding
+        return 0;
+    }
+    // pop each number on by one and keep adding at 
+    // the end of the array until it is less than 1
+    else {
+        return arr.pop() + sumOfArr(arr);
+    }
+}
+
+console.log(sumOfArr(nums))
